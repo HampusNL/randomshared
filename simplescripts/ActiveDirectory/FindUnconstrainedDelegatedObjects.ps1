d01898390f2e94d7d0db90b30f4570920c93df1c
hampus <hampus@hampusnorth.onmicrosoft.com>
2023-04-28 10:57:50 +0200

    Moved around

--- simplescripts/ActiveDirectory/FindUnconstrainedDelegatedObjects.ps1
+++
@@ -1,5 +0,0 @@
-Get-ADObject -Filter {(msDS-AllowedToDelegateTo -like '*') -or (UserAccountControl -band 0x0080000) -or (UserAccountControl -band 0x1000000)} -Properties samAccountName,msDS-AllowedToDelegateTo,servicePrincipalName,userAccountControl `
-| Select-Object DistinguishedName,ObjectClass,samAccountName,servicePrincipalName,
- @{name='DelegationStatus';expression={if($_.UserAccountControl -band 0x80000){'AllServices'}else{'SpecificServices'}}},
- @{name='AllowedProtocols';expression={if($_.UserAccountControl -band 0x1000000){'Any'}else{'Kerberos'}}},
-@{name='DestinationServices';expression={$_.'msDS-AllowedToDelegateTo'}} | Where-Object { $_.DelegationStatus -eq "AllServices" }
